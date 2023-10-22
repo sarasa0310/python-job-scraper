57482e32cfc8531687dbb4d6ba101e7a3ce68274
jimmy <dev.sarasa.0310@gmail.com>
2023-10-22 17:59:21 +0900

    job scraping 결과를 csv 파일로 다운로드 할 수 있는 기능 추가

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
 from extractors.wwr import extract_wwr_jobs
 from extractors.indeed import extract_indeed_jobs
+from file import save_to_file
 
 
 app = Flask("JobScrapper")
+cache = {}  # cache Dictionary
 
 
 @app.route("/")
@@ -11,12 +13,12 @@ def home():
     return render_template("home.html", name="jimmy")
 
 
-cache = {}  # cache Dictionary
-
-
 @app.route("/search")
 def search():
-    keyword = request.args.get("keyword")
+    keyword = request.args.get("keyword")  # 요청 파라미터 가져오기
+
+    if keyword is None:
+        return redirect("/")
 
     if keyword in cache:
         jobs = cache[keyword]  # 이미 cache에 등록되어 있는 jobs 사용
@@ -28,6 +30,21 @@ def search():
 
     return render_template("search.html",
                            keyword=keyword, jobs=jobs)
+
+
+@app.route("/export")
+def export():
+    keyword = request.args.get("keyword")
+
+    # 예외 리다이렉트
+    if keyword is None:
+        return redirect("/")
+    if keyword not in cache:
+        return redirect(f"/search?keyword={keyword}")
+
+    save_to_file(keyword, cache[keyword])  # {keyword}.csv 파일에 scarping 결과 저장
+
+    return send_file(f"{keyword}.csv", as_attachment=True)  # scraping 결과 파일 반환
 
 
 app.run(port=9000)
