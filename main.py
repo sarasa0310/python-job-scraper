d967dd725081268a92797aa7b35730374df01e52
jimmy <dev.sarasa.0310@gmail.com>
2023-10-22 14:08:45 +0900

    flask의 render_template를 활용하여 html 파일을 랜더링하는 방법 학습

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask("JobScrapper")
 
 
 @app.route("/")
 def home():
-    return "hey there!"
+    return render_template("home.html", name="jimmy")
+
+
+@app.route("/hello")
+def hello():
+    return "hello!"
 
 
 app.run(port=9000)
