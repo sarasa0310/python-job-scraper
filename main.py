108f232ea2eafa05ff20de209063d189a78a515c
jimmy <dev.sarasa.0310@gmail.com>
2023-10-22 17:20:46 +0900

    job scarping 효율성을 높이기 위해 인 메모리 cache 적용

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from extractors.wwr import extract_wwr_jobs
 from extractors.indeed import extract_indeed_jobs
 
+
 app = Flask("JobScrapper")
 
 
@@ -10,13 +11,20 @@ def home():
     return render_template("home.html", name="jimmy")
 
 
+cache = {}  # cache Dictionary
+
+
 @app.route("/search")
 def search():
     keyword = request.args.get("keyword")
 
-    wwr = extract_wwr_jobs(keyword)
-    indeed = extract_indeed_jobs(keyword)
-    jobs = wwr + indeed
+    if keyword in cache:
+        jobs = cache[keyword]  # 이미 cache에 등록되어 있는 jobs 사용
+    else:
+        wwr = extract_wwr_jobs(keyword)
+        indeed = extract_indeed_jobs(keyword)
+        jobs = wwr + indeed
+        cache[keyword] = jobs  # cache에 keyword로 등록된 jobs가 없으므로, cache에 새로 등록
 
     return render_template("search.html",
                            keyword=keyword, jobs=jobs)
